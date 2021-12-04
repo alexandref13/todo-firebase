8b0ddd55b24b4eff44a33dda9938b2d134468bc2
Alexandre Fernandes <alexfernandes199922@gmail.com>
2021-12-03 22:48:15 -0300

    added app_firebase to initialized firebase

---
+++ lib/app/app_firebase.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'app_widget.dart';
+
+void main() {
+  runApp(AppFirebase());
+}
+
+class AppFirebase extends StatefulWidget {
+  @override
+  State<AppFirebase> createState() => _AppFirebaseState();
+}
+
+final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+class _AppFirebaseState extends State<AppFirebase> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                'Não foi possível inicializar o Firebase',
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        } else {
+          return Material(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
