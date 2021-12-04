f92ac9ebe8575e2b4f8523a6a30148b8dd6303ec
Alexandre Fernandes <alexfernandes199922@gmail.com>
2021-12-03 21:36:14 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/test_challenger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_challenger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+// ignore: use_key_in_widget_constructors
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Slidy',
+      theme: ThemeData(primarySwatch: Colors.blue),
+    ).modular();
+  }
+}
